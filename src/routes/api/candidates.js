e071e817c89cdcf25bcf60ed0a26f8edd430739b
Sebastián Vicencio <sivicencio@gmail.com>
2021-11-29 23:45:25 +0100

    add candidates list endpoint (Part III first endpoint)

--- src/routes/api/candidates.js
+++ src/routes/api/candidates.js
@@ -1,5 +1,21 @@
 const KoaRouter = require('koa-router');
+const differenceInYears = require('date-fns/differenceInYears');
 
 const router = new KoaRouter();
+
+router.get('api.candidates.index', '/', async (ctx) => {
+  const candidates = await ctx.orm.candidate.findAll();
+  ctx.body = candidates.map(({
+    id, name, party, birthdate, description, photo, votes,
+  }) => ({
+    id,
+    name,
+    party,
+    years: differenceInYears(new Date(), new Date(birthdate)),
+    description,
+    photo,
+    votes,
+  }));
+});
 
 module.exports = router;
