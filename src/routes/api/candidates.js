9896f0b348ce0da49b20924b0d8aee21b4ef7fbd
Sebastián Vicencio <sivicencio@gmail.com>
2021-11-30 01:43:57 +0100

    add candidate votes and update endpoints (Part III third endpoint)

--- src/routes/api/candidates.js
+++ src/routes/api/candidates.js
@@ -1,9 +1,27 @@
+require('dotenv').config();
+
 const KoaRouter = require('koa-router');
+const jwt = require('koa-jwt');
 const differenceInYears = require('date-fns/differenceInYears');
 const format = require('date-fns/format');
 const { selectedOptions } = require('../../utils');
 
 const router = new KoaRouter();
+
+function buildCandidatePayload(candidate) {
+  const {
+    id, name, party, birthdate, description, photo, votes,
+  } = candidate;
+  return {
+    id,
+    name,
+    party,
+    years: differenceInYears(new Date(), new Date(birthdate)),
+    description,
+    photo,
+    votes,
+  };
+}
 
 function generateSlug(str) {
   return str.toLowerCase()
@@ -24,17 +42,7 @@ router.param('id', async (id, ctx, next) => {
 
 router.get('api.candidates.index', '/', async (ctx) => {
   const candidates = await ctx.orm.candidate.findAll();
-  ctx.body = candidates.map(({
-    id, name, party, birthdate, description, photo, votes,
-  }) => ({
-    id,
-    name,
-    party,
-    years: differenceInYears(new Date(), new Date(birthdate)),
-    description,
-    photo,
-    votes,
-  }));
+  ctx.body = candidates.map(buildCandidatePayload);
 });
 
 router.get('api.candidates.proposals', '/:id/proposals', async (ctx) => {
@@ -63,6 +71,28 @@ router.get('api.candidates.proposals', '/:id/proposals', async (ctx) => {
   }
 
   ctx.body = response;
+});
+
+router.use(jwt({ secret: process.env.JWT_SECRET, key: 'authData' }));
+
+router.post('api.candidates.votes', '/:id/votes', async (ctx) => {
+  const { candidate } = ctx.state;
+  await candidate.update({ votes: candidate.votes + 1 });
+  ctx.status = 201;
+  ctx.body = buildCandidatePayload(candidate);
+});
+
+router.patch('api.candidates.update', '/:id', async (ctx) => {
+  const { candidate } = ctx.state;
+  const { description, ...restBody } = ctx.request.body;
+  if (Object.keys(restBody).length) ctx.throw(400, 'Only description can be updated');
+  try {
+    await candidate.update({ description });
+  } catch (ValidationError) {
+    ctx.throw(422, ValidationError.message);
+  }
+
+  ctx.body = buildCandidatePayload(candidate);
 });
 
 module.exports = router;
